49a78ec906ac3ab3ed041d7a26c71fd42dffbdee
shipal <shivamm.ppal@gmail.com>
2022-04-20 14:28:30 +0530

    Welcome Message

---
+++ EmployeeWageStart.java
@@ -0,0 +1,10 @@
+package com.bl.employee;
+
+public class EmployeeWageStart {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Empliyee Wage Computation Program");
+
+    }
+}
