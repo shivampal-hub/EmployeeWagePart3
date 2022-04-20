afea7b8ed72d921c0dab5745d7605de654127b47
shivampal-hub <70017082+shivampal-hub@users.noreply.github.com>
2022-04-21 00:27:23 +0530

    Update EmployeeWageUC5.java

--- com/bl/employee/EmployeeWageUC5.java
+++ com/bl/employee/EmployeeWageUC5.java
@@ -10,4 +10,35 @@ public class EmployeeWageUC5 {
         wage.monthlyWage();
 
     }
+    
+    public void monthlyWage() {
+
+        for(int i = 0;i<numberOfWorkingDays;i++) {
+            totalWorkingDays++;
+
+            Random rand = new Random();
+            empCheck = rand.nextInt(3);
+
+            switch (empCheck) {
+                case 0:
+                    System.out.println("Employee Present(Full Time)");
+                    empHours = 8;
+                    break;
+                case 1:
+                    System.out.println("Employee Present(Half Time)");
+                    empHours = 4;
+                    break;
+                default:
+                    System.out.println("Employee Absent");
+                    empHours = 0;
+
+            }
+            totalEmpHours += empHours;
+            System.out.println("Day: " + totalWorkingDays + " Emp Hrs: " + empHours);
+            System.out.println();
+        }
+        monthlyWage = totalEmpHours * wagePerHour;
+        System.out.println("Monthly Wage of the Employee is: " +monthlyWage);
+
+    }
 }
